5600ea3ccb18d54f6053e2b7a61b980953dd4a21
4cylinder <ts.yew@utoronto.ca>
2016-11-01 22:51:52 -0700

    fix profile edit page styling

--- application/views/profile.php
+++ application/views/profile.php
@@ -6,7 +6,7 @@
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?=base_url();?>css/navbar_style.css">
-	
+
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -33,27 +33,32 @@
 	</div>
 </nav>
 
-<form>
-<div class="container">
-	<div class="row text-center">
-		<img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
-		<h6 id>Change Profile Photo</h6>
+<form id="userForm">
+	<div class="container text-center">
+		<div class="col-md-3">
+			<img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
+			<h6>Change Profile Photo</h6>
+			<label for="upload">
+				<span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+				<input type="file" id="newPhoto" name="newPhoto" style="display:none">
+			</label>
+		</div>
+		<div class="col-md-9">
+			<div class="row">
+				<div class="col-md-3"><label class="control-label">NAME</label></div>
+				<div class="col-md-3"><input class="form-control input-sm" placeholder="FIRST NAME" type="text" name="fName" id="fName"></div>
+				<div class="col-md-3"><input class="form-control input-sm" placeholder="LAST NAME" type="text" name="lName" id="lName"></div>
+			</div>
+			<div class="row">
+				<div class="col-md-3"><label class="control-label">EMAIL</label></div>
+				<div class="col-md-6"><input class="form-control input-sm" placeholder="EXAMPLE@DOMAIN.COM" type="text" name="email" id="email"></div>
+			</div>
+			<div class="row">
+				<div class="col-md-3"><label class="control-label">PHONE</label></div>
+				<div class="col-md-6"><input class="form-control input-sm" placeholder="123-456-7890" type="text" name="phone" id="phone"></div>
+			</div>
+		</div>		
 	</div>
-	<div class="row">
-		<div class="col-md-2"><label class="control-label">NAME</label></div>
-		<div class="col-md-2"><input class="form-control input-sm" placeholder="FIRST NAME" type="text" name="fName" id="fName"></div>
-		<div class="col-md-2"><input class="form-control input-sm" placeholder="LAST NAME" type="text" name="lName" id="lName"></div>
-	</div>
-	<div class="row">
-		<div class="col-md-2"><label class="control-label">EMAIL</label></div>
-		<div class="col-md-4"><input class="form-control input-sm" placeholder="EXAMPLE@DOMAIN.COM" type="text" name="email" id="email"></div>
-	</div>
-	<div class="row">
-		<div class="col-md-2"><label class="control-label">PHONE</label></div>
-		<div class="col-md-4"><input class="form-control input-sm" placeholder="123-456-7890" type="text" name="phone" id="phone"></div>
-	</div>
-		
-</div>
 </form>
 <script>
 $(function(){
