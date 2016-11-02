3393dd1d76edd77b6f05989511b1b170ac6da2a8
4cylinder <ts.yew@utoronto.ca>
2016-11-01 22:36:27 -0700

    make navbar styling a single common file

--- application/views/profile.php
+++ application/views/profile.php
@@ -5,10 +5,12 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="<?=base_url();?>css/navbar_style.css">
+	
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
 
@@ -35,19 +37,18 @@
 <div class="container">
 	<div class="row text-center">
 		<img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
-		<h6>Change Profile Photo</h6>
-		<input class="form-control input-small" type="file" name="profilePic" id="profilePic">
+		<h6 id>Change Profile Photo</h6>
 	</div>
-	<div class="form-group">
+	<div class="row">
 		<div class="col-md-2"><label class="control-label">NAME</label></div>
 		<div class="col-md-2"><input class="form-control input-sm" placeholder="FIRST NAME" type="text" name="fName" id="fName"></div>
 		<div class="col-md-2"><input class="form-control input-sm" placeholder="LAST NAME" type="text" name="lName" id="lName"></div>
 	</div>
-	<div class="form-group">
+	<div class="row">
 		<div class="col-md-2"><label class="control-label">EMAIL</label></div>
 		<div class="col-md-4"><input class="form-control input-sm" placeholder="EXAMPLE@DOMAIN.COM" type="text" name="email" id="email"></div>
 	</div>
-	<div class="form-group">
+	<div class="row">
 		<div class="col-md-2"><label class="control-label">PHONE</label></div>
 		<div class="col-md-4"><input class="form-control input-sm" placeholder="123-456-7890" type="text" name="phone" id="phone"></div>
 	</div>
@@ -59,10 +60,12 @@ $(function(){
 	// listener for BACK link to pop up
 	$('#backLink').click(function(e){
 		e.preventDefault();
+		console.log("back link clicked");
 	});
 	// listener for SAVE link to validate form fields and send AJAX request to back end
 	$('#saveLink').click(function(e){
 		e.preventDefault();
+		console.log("save link clicked");
 	});
 })
 </script>
