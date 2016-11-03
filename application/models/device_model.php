e65085fbe1356bec699869e27cafd3f1b52c1a15
4cylinder <ts.yew@utoronto.ca>
2016-11-02 19:24:04 -0700

    match case

--- application/models/device_model.php
+++ application/models/device_model.php
@@ -1,22 +1,27 @@
 <?php
 
 class Device_model extends CI_Model {
+	// get all devices
 	function getAll() {
 
 	}
 
+	// get single device
 	function get ($id) {
 
 	}
 
+	// delete one device
 	function delete ($id) {
 
 	}
 
+	// insert new device
 	function insert ($newDevice) {
 
 	}
 
+	// update existing device
 	function update ($deviceDetails) {
 		
 	}
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,10 +1,12 @@
 <?php
 
 class User_model extends CI_Model {
+	// get single user
 	function get($id) {
 
 	}
 
+	// update single user
 	function update($userDetails) {
 		
 	}
