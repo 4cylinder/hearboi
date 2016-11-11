2ffcb539ea5ccb4ddab55aba9047ab44d241a232
4cylinder <ts.yew@utoronto.ca>
2016-11-10 20:03:50 -0800

    shrink buttons to normal size

--- application/views/deviceForm.php
+++ application/views/deviceForm.php
@@ -14,11 +14,6 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="<?=base_url();?>js/bootstrap-switch.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    .fa {
-        font-size: 1.7em;
-    }
-    </style>
 </head>
 <body>
 
